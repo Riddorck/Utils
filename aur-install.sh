8e0a59c5e7b8f522d09cd6b2fe4c552279ba93f6
Riddorck <riddorck@gmail.com>
2018-01-28 17:35:29 +0100

    Update aur-install.sh

--- aur-install.sh
+++ aur-install.sh
@@ -1,19 +1,16 @@
 #!/bin/bash -e
 
-echo $1
-
 AUR_INSTALL(){
-  local package=${1}
-  local user='prova'
-
-  wget https://aur.archlinux.org/cgit/aur.git/snapshot/${package}.tar.gz -o /dev/null
-  tar -xf ${package}.tar.gz
-  cd ${package}
-  makepkg -fs --noconfirm
-  echo ${user} | sudo -S pacman --noconfirm -U ${package}*.pkg.tar.xz
-  cd ..
-  rm -f ${package}.tar.gz
-  rm -rf ${package}
+	local package=${1}
+	local user=${2}
+	wget https://aur.archlinux.org/cgit/aur.git/snapshot/${package}.tar.gz -o /dev/null
+	tar -xf ${package}.tar.gz
+	cd ${package}
+	makepkg -fs --noconfirm
+	echo ${user} | sudo -S pacman --noconfirm -U ${package}*.pkg.tar.xz
+	cd ..
+	rm -f ${package}.tar.gz
+	rm -rf ${package}
 }
 
-AUR_INSTALL $1
+AUR_INSTALL $1 $2
