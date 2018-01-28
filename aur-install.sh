253e88b70408139c82816aaf6155bbc1c749b406
Riddorck <riddorck@gmail.com>
2018-01-28 03:10:46 +0100

    add func

--- aur-install.sh
+++ aur-install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e
+  
+AUR-INSTALL $1
 
-local package=$1
-local user='prova'
+AUR-INSTALL(){
+  local package=${1}
+  local user='prova'
 
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/${package}.tar.gz -o /dev/null
-tar -xf ${package}.tar.gz
-cd ${package}
-makepkg -fs --noconfirm
-echo ${user} | sudo -S pacman --noconfirm -U ${package}*.pkg.tar.xz
-cd ..
-rm -f ${package}.tar.gz
-rm -rf ${package}
+  wget https://aur.archlinux.org/cgit/aur.git/snapshot/${package}.tar.gz -o /dev/null
+  tar -xf ${package}.tar.gz
+  cd ${package}
+  makepkg -fs --noconfirm
+  echo ${user} | sudo -S pacman --noconfirm -U ${package}*.pkg.tar.xz
+  cd ..
+  rm -f ${package}.tar.gz
+  rm -rf ${package}
+}
