5b4dacc08c215da59557f32fffb30926d3a9d49a
Riddorck <riddorck@gmail.com>
2018-01-28 02:46:19 +0100

    aur-install.sh

---
+++ aur-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+local package=$1
+local user='root'
+
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/${package}.tar.gz -o /dev/null
+tar -xf ${package}.tar.gz
+cd ${package}
+makepkg -fs --noconfirm
+echo ${user} | sudo -S pacman --noconfirm -U ${package}*.pkg.tar.xz
+cd ..
+rm -f ${package}.tar.gz
+rm -rf ${package}
