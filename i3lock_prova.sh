9c4303d2ab3444c83a471f061e93729b3f3d6c2c
riddorck <riddorck@gmail.com>
2020-09-20 02:56:59 +0200

    Allineamento 202009

--- i3lock_prova.sh
+++ i3lock_prova.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-w=$(xrandr | grep Screen | awk '{print $8}')
-h=$(xrandr | grep Screen | awk '{print $10}') | cut -d"," -f1
-img=img.png
-sgrana=4
-let s1=100/${sgrana}
-let s2=100*${sgrana}
-alpha=50
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale ${s1}% -scale ${s2}% /tmp/screen.png
-convert ${img} -resize ${w}x${h} -alpha set  -channel A -evaluate set ${alpha}% /tmp/screen2.png
-convert /tmp/screen.png /tmp/screen2.png -gravity center -composite -matte /tmp/screen.png
-i3lock -t -i /tmp/screen.png
+img=/tmp/i3lock-screen.png
+img2=/tmp/i3lock-screen2.png
+
+scrot ${img} -d 0.1
+convert ${img} -blur 150x50 ${img2}
+
+i3lock -t -i ${img2}
+
+sleep 0.1
+rm -f ${img}
